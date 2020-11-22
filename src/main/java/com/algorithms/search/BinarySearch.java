42c44850f8e3d3de0657f42476bef572a5f9c9ec
8h <8h@kh.octa.com>
2020-11-21 18:39:56 -0600

    Binary Search

---
+++ src/main/java/com/algorithms/search/BinarySearch.java
@@ -0,0 +1,46 @@
+package com.algorithms.search;
+
+import com.algorithms.sort.BubbleSort;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        int key = 1;
+
+        BinarySearch binarySearch = new BinarySearch();
+        int index = binarySearch.binarySearch(array, key, 0, array.length);
+
+        System.out.println("Found element at index " + index);
+
+    }
+
+    public int binarySearch(int[] array, int key, int low, int high) {
+
+        int index = Integer.MAX_VALUE;
+
+        while(low <= high) {
+            int mid = (low+high)/2;
+
+            if(mid >= array.length) {
+                return index;
+            }
+
+            if(array[mid]==key) {
+                index = mid;
+                break;
+            }
+            else if(key < array[mid]) {
+                high = mid-1;
+            }
+            else if(key > array[mid]) {
+                low = mid+1;
+            }
+        }
+
+        return index;
+
+    }
+
+}
