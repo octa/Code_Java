554bec010c3fd5458f480ba0eb24444e13fa6f55
Octa <octa@localhost.com>
2021-04-18 22:00:08 -0500

    Binary search tree

--- src/main/java/com/datastructures/tree/BinarySearchTree.java
+++ src/main/java/com/datastructures/tree/BinarySearchTree.java
@@ -42,12 +42,45 @@ public class BinarySearchTree {
 
     }
 
-    /*public Tree insertRecursively(Tree root, int value) {
+    public static Node insertRecursively(Node root, int value) {
 
-    }*/
+        if(root == null) {
+            root = new Node(value);
+            return root;
+        }
+
+        if(root.value <= value) {
+            root.right = insertRecursively(root.right, value);
+        }
+        else {
+            root.left = insertRecursively(root.left, value);
+        }
+
+        return root;
+
+    }
+
+    public static void inorder(Node root)
+    {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.value + " ");
+        inorder(root.right);
+    }
 
     public static void main(String[] args) {
 
+        int[] values = {15, 10, 20, 8, 12, 16, 25};
+
+        Node root = null;
+        for(int val: values) {
+            root = insertRecursively(root, val);
+        }
+
+        inorder(root);
     }
 
 
