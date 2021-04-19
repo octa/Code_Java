bec471b9b693283cd8166f750da9d4ac3da7f94e
Octa <octa@localhost.com>
2021-04-18 21:24:06 -0500

    Binary search tree

--- src/main/java/com/datastructures/tree/BinarySearchTree.java
+++ src/main/java/com/datastructures/tree/BinarySearchTree.java
@@ -1,17 +1,50 @@
 package com.datastructures.tree;
 
-import com.datastructures.tree.Tree;
-
 public class BinarySearchTree {
 
-    public Tree insertIteratively(int value) {
+    Node root;
 
+    public Node insertIteratively(Node root, int value) {
+
+        if(root == null) {
+            root = new Node();
+            root.value = value;
+            return root;
+        }
+
+        Node curr_node = root;
+
+        while(true) {
+
+            if(curr_node.value <= value) {
+
+                if(curr_node.right != null) {
+                    curr_node = curr_node.right;
+                }
+                else {
+                    curr_node.right = new Node(value);
+                    break;
+                }
+            }
+            else {
+                if(curr_node.left != null) {
+                    curr_node = curr_node.left;
+                }
+                else {
+                    curr_node.left = new Node(value);
+                    break;
+                }
+            }
+
+        }
+
+        return root;
 
     }
 
-    public Tree insertRecursively(Tree root, int value) {
+    /*public Tree insertRecursively(Tree root, int value) {
 
-    }
+    }*/
 
     public static void main(String[] args) {
 
---
+++ src/main/java/com/datastructures/tree/Node.java
@@ -0,0 +1,16 @@
+package com.datastructures.tree;
+
+public class Node {
+    int value;
+    Node parent;
+    Node left;
+    Node right;
+
+    Node() {
+
+    }
+
+    Node(int value) {
+        this.value = value;
+    }
+}
